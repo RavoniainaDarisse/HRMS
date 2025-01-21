0e9098d7b3625ff483fc11f65f4cfe0b59aeef83
RavoniainaDarisse <ravoniainaheritiana@gmail.com>
2025-01-21 15:11:58 +0300

    file

--- obj/Debug/net8.0/HRMS.AssemblyInfo.cs
+++ obj/Debug/net8.0/HRMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bf92c4f9348357c98c1388ab9118dd9e48e0010")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
